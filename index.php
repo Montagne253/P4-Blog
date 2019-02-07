108b8f81dae662355599cf968d622670a065d3e9
Montagne <montagne.malcz.o@gmail.com>
2019-02-07 17:25:06 +0100

    New Nav-bar (nouvelle architecture site)

--- index.php
+++ index.php
@@ -13,30 +13,34 @@ $bdd = new PDO('mysql:host=localhost;dbname=p4;charset=utf8', 'root', 'Dj253kolo
         <title>Jean Laroche | Blog des écrivains</title>
     <link href="projet4.css" rel="stylesheet" /> 
     <link href="style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
 
    
     <body>
       
-    <header class="header">
-        <div class="header__element">
-        <div class="title">
-        <h2 style="text-align: center">JEAN FORTEROCHE | ÉCRIVAIN.</h2>
-            <p>Bienvenue sur mon blog !</p>
-        </div>
-            <nav class="navigation">       
-                <ul class="navigation__menu--header">
-                        <li class="navigation__menu__id--header">
-                            <a class="navigation__link" href="#index.php">Accueil</a>
-                        </li>
-                        <li class="navigation__menu__id--header">
-                            <a class="navigation__link" href="billet.php">Billets</a>
-                        </li>
-                       
-                </ul>
-            </nav>
-        </div>
-    </header>
+<header >
+   
+
+    <div class="container-fluid">
+
+            <div class="pos-f-t">
+                <div class="collapse" id="navbarToggleExternalContent">
+                <a class="navbar-brand" href="connexion.php">Admin</a>
+                </div>
+                <nav class="navbar navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="title">
+                        <h2 >JEAN FORTEROCHE | ÉCRIVAIN.</h2>
+                        <p>Bienvenue sur mon blog !</p>
+                    </div>
+                </nav>
+            </div>
+    </div>
+                
+</header>
     <br>
     <br>
     <br>
@@ -97,6 +101,8 @@ $bdd = new PDO('mysql:host=localhost;dbname=p4;charset=utf8', 'root', 'Dj253kolo
         
 
 
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>   
 </body>
 </html>
--- profil.php
+++ profil.php
@@ -21,9 +21,32 @@ if(isset($_SESSION['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="projet4.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <title>Profil</title>
 </head>
 <body>
+<header >
+   
+
+<div class="container-fluid">
+
+        <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+            <a class="navbar-brand" href="connexion.php">Admin</a>
+            </div>
+            <nav class="navbar navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="title">
+                    <h2 >JEAN FORTEROCHE | ÉCRIVAIN.</h2>
+                    <p>Bienvenue sur mon blog !</p>
+</div>
+            </nav>
+        </div>
+</div>
+                
+</header>
 
     <div id="profil" align="center">
         <h2>Profil de <?php echo $userinfo['pseudo']; ?></h2>
@@ -64,6 +87,9 @@ if(isset($_SESSION['id'])) {
         ?>
     
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>   
     
 </body>
 </html>
