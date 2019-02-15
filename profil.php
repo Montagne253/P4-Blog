a3d2bc599344347d158f6652030816d8d5379f17
Montagne <montagne.malcz.o@gmail.com>
2019-02-15 12:54:19 +0100

    New Link

--- profil.php
+++ profil.php
@@ -39,8 +39,8 @@ if(isset($_SESSION['id'])) {
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="title">
-                    <h2 >JEAN FORTEROCHE | ÉCRIVAIN.</h2>
-                    <p>Bienvenue sur mon blog !</p>
+                    <h2>JEAN FORTEROCHE | ÉCRIVAIN.</h2>
+                    <p>Administration</p>
                 </div>
             </nav>
         </div>
@@ -66,31 +66,35 @@ if(isset($_SESSION['id'])) {
         
         {
         ?>
-<h6>Atelier</h6>
-<table class="table table-dark">
-  
+
+</div>
+<h6>ATELIER</h6>
+<table class="table table-light">
   <tbody>
     <tr>
-      <th scope="row">Éditer</th>
-      <td><a class="btn btn-primary" role="button" href="editbillet.php">Billets</a></td>
-      <td><a class="btn btn-primary" role="button" href="editerprofil.php">Profil</a></td>
+      <th class="creer" scope="row">Créer</th>
+      <td><a role="button" href="redaction.php" class="btn btn-primary">Billets</a></td>
+      <td><a role="button" href="signin.php" class="btn btn-primary">Nouveau compte</a></td>
     </tr>
     <tr>
-      <th scope="row"><img src=""/>Créer un nouveau billet</th>
-      <td><a role="button" href="redaction.php" class="btn btn-primary">Créer</a></td>
+      <th class="editer" scope="row">Éditer</th>
+      <td><a class="btn btn-primary" role="button" href="editbillet.php">Billets</a></td>
+      <td><a class="btn btn-primary" role="button" href="editerprofil.php">Mon profil</a></td>
     </tr>
   </tbody>
 </table>
 
+<div class="nav__exit">
+        <a class="btn btn-primary_nav" role="button" href="index.php">Retour blog</a>
         <br>
-        <a class="navigation__link__1" href="index.php">Retour au blog</a>
-        <br>
-        <a class="navigation__link__1" href="deconnexion.php">Deconnexion</a>
+        <a class="btn btn-primary_nav" role="button" href="deconnexion.php">Deconnexion</a>
         <?php
         }
         ?>
+<div>
+
     
-    </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>   
