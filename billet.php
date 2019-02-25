3340eb3ee8e55b030f9236056960fc3557481300
Montagne <montagne.malcz.o@gmail.com>
2019-02-25 17:06:03 +0100

    Nouvelle page :  affiche le billet en entier au clique de "lire la suite"

--- billet.php
+++ billet.php
@@ -5,35 +5,15 @@ session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=p4;charset=utf8', 'root', 'Dj253kolo932018');
 
 ?>
-<?php
 
- 
 
-function truncate($string, $max_length = 30, $replacement = '', $trunc_at_space = false)
-
-{
-
-    $max_length -= strlen($replacement);
-
-    $string_length = strlen($string);
-
-    if($string_length <= $max_length)
-
-    return $string;
-
-    if( $trunc_at_space && ($space_position = strrpos($string, ' ', $max_length-$string_length)) )
-
-    $max_length = $space_position;
-    return substr_replace($string, $replacement, $max_length);
-}
-?>
 <?php
 
 
-         // On récupère les billets
-         $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billet ORDER BY id DESC');
+$req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billet WHERE id = ?');
+$req->execute(array($_GET['billet']));
 
-            
+
          while ($donnees = $req->fetch())
           {
 
@@ -60,9 +40,6 @@ function truncate($string, $max_length = 30, $replacement = '', $trunc_at_space 
 
             <div class="pos-f-t">
                 <div class="collapse" id="navbarToggleExternalContent">
-                <a class="navbar-brand" href="connexion.php">Admin</a>
-                </div>
-                <div class="collapse" id="navbarToggleExternalContent">
                 <a class="navbar-brand" href="billet.php">Mes billets</a>
                 </div>
                 <div class="collapse" id="navbarToggleExternalContent">
@@ -82,81 +59,54 @@ function truncate($string, $max_length = 30, $replacement = '', $trunc_at_space 
                 
 </header>
 
+<div class="news">
+    <h3>
+                 
+        <?php echo htmlspecialchars($donnees['titre']); ?>
+        <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+    </h3>
+              
+    <p>
 
-<div class="container-fluid">
-
-
-<table class="table table-hover table-light">
-
-<thead>
-    <tr class="header_tab">
-        <td scope="col" >Titre</td>
-        <td scope="col" >Date</td>
-        <td scope="col" >Billet</td>
-        <td></td>
-        <td></td>
-    </tr>
-</thead>
-<tbody>
-    
 <?php
-
-while ($donnees = $req->fetch())
-    
-{
-?>
-    <tr>
-        <td scope="row"><?php echo htmlspecialchars($donnees['titre']); ?></td>
-        <td><?php echo $donnees['date_creation_fr']; ?></td>
-        <td>
-            <?php
  
-            $str = $donnees['contenu'];
-            $str = truncate($str, 50, '...', true);
-            echo  ' " '  .  $str . ' " '   ;
-            // Suspendisse at magna non lectus...
-                        // On affiche le contenu du billet
-                    // echo nl2br(htmlspecialchars($donnees['contenu']));
-            ?>
-        </td>
-        <td>
-        <a class="btn btn-primary_nav_2" href="billetById.php?billet=<?php echo $donnees['id']; ?>">Lire la suite</a>
-        </td>   
-        <td>
-        <em><a class="btn btn-primary_nav_2" href="comment.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
-        </td>
-    </tr>
-  </tbody>
-
+ 
   
-<?php
-    } // Fin de la boucle des billets
-          
-    
+ 
+ 
+ 
+
+
+
+              // On affiche le contenu du billet
+         echo nl2br(htmlspecialchars($donnees['contenu']));
 ?>
-       
-       
+        <br />
+        <em><a href="comment.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+    </p>            
+</div>
+          <?php
+          } // Fin de la boucle des billets
+          
+          $req->closeCursor();
+          ?>
+
+<br>
+<br>
+<br>
+
+<div id="scrollUp">
+<a class="top" href="#top"><img  class="scrollUp" src="img/to_top.png"/></a>
+</div>
+<div class="nav__exit">
+<a class="btn btn-primary_nav" role="button" href="index.php">Retour au blog</a>
 </div>
 
 
-<?php
-    } // Fin de la boucle des billets
-          
-    $req->closeCursor();
-?>
-  
 
-
-
-
-
-
-
-
+<script src="to-top.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>   
 </body>
 </html>        
-         
-
